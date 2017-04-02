148d6e3b3e5ad6e0c4d6c8719814e513e282e322
polo <slafe29@yandex.com>
2017-04-02 05:40:26 +0000

    bismillah

--- sampan.js
+++ sampan.js
@@ -8,14 +8,23 @@ var driver = new webdriver.Builder()
     .build();
 driver.manage().window().setSize(1000, 1000);
 driver.get('http://www.c9.io/login');
-driver.findElement(By.id('id-username')).sendKeys('slafe29@yandex.com');
+driver.findElement(By.id('id-username')).sendKeys('slafe1@yandex.com');
 driver.findElement(By.id('id-password')).sendKeys('plokotoklucu');
 driver.findElement(By.css("button[label='Sign in']")).click();
 driver.sleep(10000);
-driver.get('https://ide.c9.io/spiritbro/sampan');
+driver.get('https://ide.c9.io/kolotibablo/semaphore3');
 driver.sleep(20000)
  driver.wait(until.elementLocated(By.xpath("(//textarea[@class='ace_text-input'])[2]")) , 5000);
-driver.findElement(By.xpath("(//textarea[@class='ace_text-input'])[2]")).sendKeys("ps -ef");
+driver.findElement(By.xpath("(//textarea[@class='ace_text-input'])[2]")).sendKeys("git clone https://github.com/lokimonoton/durant.git");
+driver.findElement(By.xpath("(//textarea[@class='ace_text-input'])[2]")).sendKeys(webdriver.Key.ENTER);
+driver.sleep(10000)
+driver.findElement(By.xpath("(//textarea[@class='ace_text-input'])[2]")).sendKeys("npm install");
+driver.findElement(By.xpath("(//textarea[@class='ace_text-input'])[2]")).sendKeys(webdriver.Key.ENTER);
+driver.sleep(50000)
+driver.findElement(By.xpath("(//textarea[@class='ace_text-input'])[2]")).sendKeys("node xserver.js &");
+driver.findElement(By.xpath("(//textarea[@class='ace_text-input'])[2]")).sendKeys(webdriver.Key.ENTER);
+driver.sleep(10000)
+driver.findElement(By.xpath("(//textarea[@class='ace_text-input'])[2]")).sendKeys("node lakim.js &");
 driver.findElement(By.xpath("(//textarea[@class='ace_text-input'])[2]")).sendKeys(webdriver.Key.ENTER);
 driver.sleep(5000)
 driver.takeScreenshot().then(
---
+++ xserver.js
@@ -0,0 +1,8 @@
+const exec = require('child_process').exec;
+exec('java -jar selenium-server-standalone-2.45.0.jar', (err, stdout, stderr) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(stdout);
+});
