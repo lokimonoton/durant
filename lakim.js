5ce243421b7c31830de3c86b837276d307c81a01
sampan <slafe1@yandex.com>
2017-04-10 11:46:09 +0000

    bismillah

--- lakim.js
+++ lakim.js
@@ -1,8 +1,20 @@
 var express = require('express')
 var app = express()
+//untuk menjadikan json
+var bodyParser = require('body-parser')
+var jsonParser = bodyParser.json()
+var FB = require('fb');
+app.get('/goal', function (req, res) {
+  res.sendFile(__dirname+'/panda.html')
+})
 
 app.get('/', function (req, res) {
-  res.send('ciki')
+  res.sendFile(__dirname+'/lakim.html')
+})
+//webhook mendapatkan notifikasi facebook
+app.post('/facebook', jsonParser, function (req, res) {
+  res.send("berhasil").status(200)
+
 })
 
 app.listen(process.env.PORT, function () {
--- xserver.js
+++ xserver.js
@@ -6,7 +6,7 @@ exec('java -jar selenium-server-standalone-2.45.0.jar', (err, stdout, stderr) =>
   }
   console.log(stdout);
 });
-exec('node lakim.js', (err, stdout, stderr) => {
+exec('nodemon lakim.js', (err, stdout, stderr) => {
   if (err) {
     console.error(err);
     return;
