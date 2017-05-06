fa46b0977bc658ba7daa2bd7d0489a71c6af0e2a
sampan <slafe1@yandex.com>
2017-05-06 02:12:33 +0000

    bismillah

--- kolotibablo.js
+++ kolotibablo.js
@@ -1,11 +1,4 @@
-var webdriver = require('selenium-webdriver'),
-    By = webdriver.By,
-    until = webdriver.until;
-
-var driver = new webdriver.Builder()
-    .forBrowser('firefox')
-    // .usingServer('http://localhost:4444/wd/hub')
-    .build();
-driver.manage().window().setSize(1000, 1000);
-driver.get('https://www.codenvy.io');
-driver.quit()
+var koneksi=require('./koneksi')
+koneksi.cari("codenvy",{},data=>{
+   console.log(data)
+})
--- selakimo.js
+++ selakimo.js
@@ -1,5 +1,5 @@
 const exec = require('child_process').exec;
-  exec('./jikaberhasil', (err, stdout, stderr) => {
+  exec('source jikaberhasil', (err, stdout, stderr) => {
     if (err) {
       console.error(err);
       return;
