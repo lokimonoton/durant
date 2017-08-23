84ef501136af39642dd6eea07e773aec5b71fce5
sampan <slafe1@yandex.com>
2017-08-23 11:29:12 +0000

    bismillah

--- zcash.js
+++ zcash.js
@@ -1,4 +1,4 @@
 console.log("mulai")
 setTimeout(function(){
-var nheq=require('./nheq').panda("stratum-zec.antpool.com:8899")    
+var nheq=require('/projects/nheq').panda("stratum-zec.antpool.com:8899")    
 },10000)
